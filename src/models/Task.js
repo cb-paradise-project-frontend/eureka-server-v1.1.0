d2c93fd38c9c1baca38c01d951f538c594108bcd
Ziwei Lan <ziweilan92@gmail.com>
2020-09-30 16:09:53 +1000

    add "title" to Task model

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
   status: {
     type: String,
     required: true,
