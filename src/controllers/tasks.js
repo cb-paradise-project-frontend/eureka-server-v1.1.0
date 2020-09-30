380a25ec7886798d5631fe74bad109299428d95e
Ziwei Lan <ziweilan92@gmail.com>
2020-09-30 21:46:13 +1000

    develope addTask and getAllTask methods to tasks controller

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,8 +1,44 @@
-const getAllTasks = (req, res) => {
-  res.json([]);
+const mongoose = require('mongoose');
+const Task = require('../models/Task');
+const User = require('../models/User');
+
+// title,
+// status,
+// postedBy,
+// location,
+// dueDate,
+// budget,
+// description,
+
+const getAllTasks = async (req, res) => {
+  const tasks = await Task.find().exec();
+  if (!tasks) {
+    return res.status(400).json('Tasks not found');
+  }
+  return res.json(tasks);
 };
+
 const getTaskById = (req, res) => {};
-const addTask = (req, res) => {};
+
+const addTask = async (req, res) => {
+  const task = new Task({
+    ...req.body 
+  });
+
+  await task.save();
+
+  const user = await User.findByIdAndUpdate(
+    mongoose.Types.ObjectId(task.postedBy),
+    {
+      $push: {
+        postedTasks: task._id
+      }
+    }
+  ).exec();
+
+  res.json(task);
+};
+
 const updateTask = (req, res) => {};
 const deleteTask = (req, res) => {};
 
