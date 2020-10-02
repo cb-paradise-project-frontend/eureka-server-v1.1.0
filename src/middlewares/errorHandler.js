bf560f4c039eaf71ca0b87f0cc721b1d1d04a9b8
Ziwei Lan <ziweilan92@gmail.com>
2020-10-02 16:19:52 +1000

    adjust error handling logic to fix the message returning issue

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,12 @@
 const errorHandler = (error, req, res, next) => {
-  if (error.name === 'ValidationError') {
-    return res.json(error.details);
-  }
+  const { title, _message } = error;
+  console.log(_message);
+
+  // if (error.name === 'ValidationError') {
+  //   return res.json(error.details);
+  // }
+
+  return res.json(_message);
 }
 
 module.exports = errorHandler;
