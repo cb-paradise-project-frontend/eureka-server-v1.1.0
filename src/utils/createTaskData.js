461d587fd85c667e7bed9747926073900b786de4
Ziwei Lan <ziweilan92@gmail.com>
2020-10-08 14:14:13 +1100

    refactor createTaskData

--- src/utils/createTaskData.js
+++ src/utils/createTaskData.js
@@ -1,10 +1,12 @@
 const Task = require('../models/Task');
+const User = require('../models/User');
+const toObjectId = require('./toObjectId');
 
 const testData = {
   title: 'Roof repair',
   status: 'OPEN',
   budget: '120',
-  postedBy: "5f742e1c1092f55f18a7ac00",
+  postedBy: "5f7e816e2ae95e573c9d0814",
   location: 'Kurunjang VIC 3337, Australia',
   dueDate: "Wed Sep 30 2020 21:34:17 GMT+1000 (Australian Eastern Standard Time)",
   description: `-remove existing bamboo fence - supply and install new fence (open for ideas)
@@ -37,5 +39,14 @@ module.exports = async function createTaskData(size) {
   await dataArray.map(async (data) => {
     const task = new Task({ ...data });
     await task.save();
+
+    const user = await User.findByIdAndUpdate(
+      toObjectId(task.postedBy),
+      {
+        $push: {
+          postedTasks: task._id
+        }
+      }
+      ).exec();
   });
 }
