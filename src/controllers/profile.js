d678b249d0bd475cde316feadad6059684f4e46b
Ziwei Lan <ziweilan92@gmail.com>
2020-10-15 17:10:28 +1100

    add user updating logic to add profile

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,15 +1,25 @@
 const HttpError = require('../utils/HttpError');
+const User = require('../models/User');
 const { sendResult } = require('../utils/sendResponse');
 const Profile = require('./../models/Profile');
 
 const addProfile = async (req, res) => {
+  const { user } = req.body;
+
+  const matchUser = await User.findById(user).exec();
+
+  if (matchUser.profile) throw new HttpError(403, 'Profile already exist');
+
   const profile = new Profile({
     ...req.body
   });
 
   if (!profile) throw new HttpError(400, 'Invalid profile input');
 
+  matchUser.profile = profile._id;
+
   await profile.save();
+  await matchUser.save();
 
   return sendResult(res, profile);
 };
