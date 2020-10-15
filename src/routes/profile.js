62e87534b0f61d4c97cd26334393ee6426c17573
Ziwei Lan <ziweilan92@gmail.com>
2020-10-15 18:49:08 +1100

    bind getProfileByUserId to route

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,11 +3,13 @@ const express = require('express');
 const {
   addProfile,
   getAllProfile,
+  getProfileByUserId,
 } = require('./../controllers/profile');
 
 const router = express.Router();
 
 router.get('/', getAllProfile);
+router.get('/:id', getProfileByUserId);
 router.post('/', addProfile);
 
 module.exports = router;
