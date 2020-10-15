c47273a8815a55f0fec282e1fed3a75b475196c1
Ziwei Lan <ziweilan92@gmail.com>
2020-10-15 16:42:59 +1100

    add profile router

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const taskRoute = require('./routes/tasks');
 const userRoute = require('./routes/users');
+const profileRoute = require('./routes/profile');
 
 const router = express.Router();
 
 router.use('/tasks', taskRoute);
 router.use('/users', userRoute);
+router.use('/profiles', profileRoute);
 
 module.exports = router;
