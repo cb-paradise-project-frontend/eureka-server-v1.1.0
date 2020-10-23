652cd38d2820630c10cd03cf203516691ff3d0f8
Changhong Lin <changhong.linst@gmail.com>
2020-10-23 17:09:01 +1000

    add more fn in task controller

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -5,9 +5,12 @@ const auth = require('../middlewares/auth');
 const {
   getAllTasks,
   getTaskByUserId,
+  getTaskByCategory,
   getTaskById,
   addTask,
+  assignTask,
   updateTask,
+  completeTask,
   addComment,
   makeOffer,
   deleteTask
@@ -17,12 +20,15 @@ const router = express.Router();
 
 router.get('/', getAllTasks);
 router.get('/private', auth , getTaskByUserId);
+router.get('/category/:category', getTaskByCategory);
 
 router.get('/:id', getTaskById); //using for test
 
 router.post('/', auth, addTask);
 
 router.put('/:id', updateTask);
+router.put('/assign/:taskId', auth, assignTask); //patch
+router.put('/complete/:taskId', auth, completeTask);
 router.put('/comment/:id', auth, addComment);
 router.put('/offer/:id', auth, makeOffer);
 
