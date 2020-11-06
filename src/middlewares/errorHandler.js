584138bbd96eadc0f3c3624324188d7c2c664ad5
Wayne JSNoob <wenpeijs@gmail.com>
2020-11-06 23:24:11 +1100

    ✏️ removed all clg

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,11 +2,6 @@ const { sendError } = require('../utils/sendResponse');
 
 const errorHandler = (error, req, res, next) => {
   const { isHttpError, code, message } = error;
-  console.log(message);
-
-  // if (error.name === 'ValidationError') {
-  //   return res.json(error.details);
-  // }
 
   if (!message) return sendError(res, 500, 'Unknown error.');
 
